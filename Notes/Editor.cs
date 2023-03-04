6804e8e3f2de4769d268caa7072cd8ad329ce96e
tatia2501 <catatat@yandex.ru>
2023-03-04 23:12:59 +0300

    feat: buttons and forms creating

---
+++ Notes/Editor.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Notes
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+        
+        public Editor(Form1 f)
+        {
+            InitializeComponent();
+        }
+    }
+}
