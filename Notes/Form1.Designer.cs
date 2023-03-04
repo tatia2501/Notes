05f316b56fe418dea30f31e3fdf6e474e8283eca
tatia2501 <catatat@yandex.ru>
2023-03-04 23:42:55 +0300

    feat: add button functionality

--- Notes/Form1.Designer.cs
+++ Notes/Form1.Designer.cs
@@ -30,44 +30,30 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 7);
+            this.button1.Location = new System.Drawing.Point(7, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 28);
+            this.button1.Size = new System.Drawing.Size(85, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(20, 68);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 124);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Заметка 1";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 498);
-            this.Controls.Add(this.button2);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(842, 618);
             this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "Notes";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
         }
-
-        private System.Windows.Forms.Button button2;
 
         private System.Windows.Forms.Button button1;
 
--- Notes/Form1.cs
+++ Notes/Form1.cs
@@ -12,6 +12,15 @@ namespace Notes
 {
     public partial class Form1 : Form
     {
+        private int _noteHeight = 120;
+        private int _noteWidth = 150;
+        private int _notePositionX = 20;
+        private int _notePositionY = 70;
+        private int _notePositionXChange = 180;
+        private int _notePositionYChange = 150;
+        private int _formWidth = 1000;
+        private int _initialXLocation = 20;
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +28,34 @@ namespace Notes
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Button newBtn = new Button();
+            newBtn.Text = "Новая заметка";
+            newBtn.Size = new Size(_noteWidth, _noteHeight);
+            newBtn.Location = new Point(_notePositionX, _notePositionY);
+            newBtn.Click += new EventHandler(button2_Click);
+            newBtn.UseVisualStyleBackColor = false;
+            this.Controls.Add(newBtn);
         }
         
         private void button1_Click(object sender, EventArgs e)
         {
+            Button newBtn = new Button();
+            newBtn.Text = "Новая заметка";
+            newBtn.Size = new Size(_noteWidth, _noteHeight);
+            if (_notePositionX + _notePositionXChange < _formWidth)
+            {
+                newBtn.Location = new Point(_notePositionX + _notePositionXChange, _notePositionY);
+                _notePositionX += _notePositionXChange;
+            }
+            else
+            {
+                newBtn.Location = new Point(_initialXLocation, _notePositionY + _notePositionYChange);
+                _notePositionX = _initialXLocation;
+                _notePositionY += _notePositionYChange;
+            }
+            newBtn.Click += new EventHandler(button2_Click);
+            newBtn.UseVisualStyleBackColor = false;
+            this.Controls.Add(newBtn);
         }
 
         private void button2_Click(object sender, EventArgs e)
