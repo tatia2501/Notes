c24e46e4ca398a9129defc723d2cfd4112140b13
tatia2501 <catatat@yandex.ru>
2023-03-07 23:57:10 +0300

    feat: connection to db

--- Notes/Form1.cs
+++ Notes/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace Notes
 {
     public partial class Form1 : Form
     {
+        SqlConnection connection;
+        SqlCommand command;
+        
         private int _noteHeight = 60;
         private int _noteWidth = 200;
         private int _notePositionX = 20;
@@ -23,12 +27,33 @@ namespace Notes
         public Form1()
         {
             InitializeComponent();
+            
+            connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=LAPTOP-02135ROM\SQLEXPRESS;Initial Catalog=Notes;Integrated Security=SSPI;";
+            command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             Button newBtn = new Button();
-            newBtn.Text = "Новая заметка";
+            string title="55";
+
+            command.CommandText = "select * from NotesText where ip=0;";
+            connection.Open();
+            SqlDataReader reader= command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    title = reader.GetString(1);
+                    var text = reader.GetString(2);
+                }
+            }
+            connection.Close();
+
+            newBtn.Text = title;
             newBtn.Size = new Size(_noteWidth, _noteHeight);
             newBtn.Location = new Point(_notePositionX, _notePositionY);
             newBtn.Click += new EventHandler(button2_Click);
